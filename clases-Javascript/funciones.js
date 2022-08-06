3da34326768815f346781f6781c3d97f63766bb1
andresh01 <andres85684@outlook.com>
2022-08-06 12:07:13 -0500

    chistes desde api con jason

---
+++ clases-Javascript/funciones.js
@@ -0,0 +1,23 @@
+function mensajes(){
+    console.log("Hola")
+    console.log("Soy una funcion ")
+    console.log("Sin parametros")
+}
+
+mensajes()
+
+/**/ 
+
+function nombres(nombre){
+    console.log("Hola soy "+nombre)
+}
+
+nombres('andres')
+
+/** */
+
+function multiplicacion(num1,num2){
+    return num1*num2
+}
+
+console.log(multiplicacion(2,3))
